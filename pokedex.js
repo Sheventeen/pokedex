8e722ee84f24d8e654acf6e39ecef75875b4013d
Princepreet <Princepreet2021@gmail.com>
2024-08-13 04:21:34 -0400

    implemented fetch using promise.all and added button functionallity

--- pokedex.js
+++ pokedex.js
@@ -1,24 +1,35 @@
+// gathering html element ids to work with and dynamically update as we nativate the program
 const pokedex = document.getElementById("pokedex");
 const display = document.getElementById("displayContainer");
+document.getElementById("searchButton").addEventListener("click", searchPokemon);
+document.getElementById("randomButton").addEventListener("click",randomPokemon);
+document.getElementById("searchBox").addEventListener("keydown", function(event){
+    if(event.key === "Enter")searchPokemon(); 
+});
+
+const pokeArr = [];
+const promiseArr = [];
 
 document.addEventListener('DOMContentLoaded', async()=>{
-    const pokeArr = [];
-
+    
     for (let pokemonId =1; pokemonId<152;pokemonId++){
         const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-
-        await fetch(url)
-        .then(resp=>{
-            if(!resp.ok) throw new Error("Not a valid response");
-            return resp.json();
-        })
-        .then(pokemon=>{
+        promiseArr.push(fetch(url).then(res => {
+            if(!res.ok) throw new Error("Error occured when trying to fetch pokemons api");
+            return res.json();
+        }));
+    }
+        try{
+           const pokeData = await Promise.all(promiseArr);
+            
+           pokeData.forEach((pokemon, index) => {
+            const pokemonId = index + 1;
             const pokeObj = {
                 url: `https://pokeapi.co/api/v2/pokemon/${pokemonId}`,
                 name: pokemon.name,
                 id: pokemonId,
                 image: pokemon.sprites.front_default
-            }; 
+            };
             pokeArr.push(pokeObj);
 
             const newElement = document.createElement("li");
@@ -32,15 +43,19 @@ document.addEventListener('DOMContentLoaded', async()=>{
             newElement.textContent = pokeObj.name;
             newElement.appendChild(pokemonImage);
             newElement.setAttribute("data-url", pokeObj.url);
-
-            pokedex.appendChild(newElement);
             newElement.addEventListener("click",onClick);
-            
-        })};
-        console.log(pokeArr);
-});
 
-
+            pokedex.appendChild(newElement); 
+           });
+           console.log(pokeArr);
+            const now = new Date();
+            const currentDateTime = now.toLocaleString();
+            console.log(currentDateTime);
+        }
+        catch(error){
+            console.error("failed",error);
+            }
+        });
 
 function onClick(event){
     const itemName = event.currentTarget.name;
@@ -53,5 +68,21 @@ function displayPokemon(pokemon, pokemonId){
     const url = `displayPokemon.html?name=${encodeURIComponent(pokemon)}&id=${encodeURIComponent(pokemonId)}`;
     window.location.href = url;
 }
+function searchPokemon(){
+    const input = document.getElementById("searchBox").value.toLowerCase().trim();
+    const pokemonItem = document.getElementsByTagName("li");
 
+    for(let i=0;i < pokemonItem.length;i++){
+        const currPokemon = pokemonItem[i].name.toLowerCase();
 
+        if(currPokemon.includes(input))
+            pokemonItem[i].style.display = "";
+        else pokemonItem[i].style.display = "none";
+    }
+}
+function randomPokemon(){
+    const max = document.getElementsByTagName("li").length;
+    const id = Math.floor(Math.random() * (max) + 1);
+    const pokemon = pokeArr[id-1].name;
+    displayPokemon(pokemon,id);
+}
